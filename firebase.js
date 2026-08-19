d621f7f1108f3b77d29fdbdee21e6f3ce5ce2b40
somchandtrust-boo <somchandtrust@gmail.com>
2026-08-19 11:56:10 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// अपनी Firebase Console से values यहाँ डालें.
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBBoLatjHkL56Lm0viPXcV91xcLewrosmI",
+  authDomain: "cbrnd-location-tracker.firebaseapp.com",
+  projectId: "cbrnd-location-tracker",
+  storageBucket: "cbrnd-location-tracker.firebasestorage.app",
+  messagingSenderId: "513696622411",
+  appId: "1:513696622411:web:52df6f0833380f76802ace",
+  measurementId: "G-GCDRNNZQVY"
+};
